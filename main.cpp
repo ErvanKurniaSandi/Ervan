5f9361fdb19469e10408e64c89591109269efd3e
ErvanKurniaSandi <149492430+ErvanKurniaSandi@users.noreply.github.com>
2023-11-01 09:42:44 +0700

    Add files via upload
    Tugas DPP 1-11-2023

---
+++ main.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+int main() {
+    int kesamping = 3; 
+    int kebawah = 3; 
+  
+    for (int s = 1 ; s <= kesamping; s++) {
+        for (int b = 1 ; b <= kebawah; b++) {
+          cout <<"|_|";
+        }
+          cout <<"\n";
+    }
+
+    return 0;
+}
